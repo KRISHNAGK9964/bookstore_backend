5b1b7ed57d0ade7f6b1a06582e51dd91770a5fc4
KRISHNA K <kkrishna22572@gmail.com>
2024-02-09 16:43:47 +0530

    update production url

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173', // Replace with your frontend domain
+    origin: 'https://bookstore-frontend-2hjc.vercel.app', // Replace with your frontend domain
     credentials: true,
     // Other CORS options as needed
   })
